3a3a0f80dfce224440cb6f15447435df301da1ed
mnc1423 <41675771+mnc1423@users.noreply.github.com>
2023-11-15 14:58:00 +0900

    updates to code

--- elastic_handler.py
+++ elastic_handler.py
@@ -1,19 +1,29 @@
 from elasticsearch import Elasticsearch, helpers
 from config import config
 
+
 class esHandler:
     def __init__(self):
-        es_host=config.get_es_server()
-        es_port=config.get_es_port()
-        self.es_client = Elasticsearch(
-            "localhost",
-            port=es_port
-        )
+        es_host = config.get_es_server()
+        es_port = config.get_es_port()
+        self.es_client = Elasticsearch("localhost", port=es_port)
         self.es_index = config.get_es_index()
-    
+
     def create_doc(self, data):
         doc = {
             "_op_type": "create",
             "_index": self.es_index,
-            "_id": data['_id'] 
+            "_id": data["_id"],
+            "url_list": data["url_list"],
+            "vid_title": data["vid_title"],
+            "publishedAt": data["publishedAt"],
+            "channelTitle": data["channelTitle"],
+            "channelId": data["channelId"],
         }
+        return doc
+
+    def createInputData(self, _list):
+        inputList = []
+        for docs in _list:
+            doc = self.create_doc(docs)
+            inputList.append(doc)
--- youtube_api.py
+++ youtube_api.py
@@ -4,50 +4,56 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import re
 from urlextract import URLExtract
+from pprint import pprint
+
 
 class Youtube_API:
     def __init__(self):
-        DEVELOPER_KEY =  config.get_api_key()
-        YOUTUBE_API_SERVICE_NAME = 'youtube'
-        YOUTUBE_API_VERSION = 'v3'
-        self.youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-                        developerKey=DEVELOPER_KEY)
+        DEVELOPER_KEY = config.get_api_key()
+        YOUTUBE_API_SERVICE_NAME = "youtube"
+        YOUTUBE_API_VERSION = "v3"
+        self.youtube = build(
+            YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY
+        )
         parser = argparse.ArgumentParser()
-        parser.add_argument('--q', help='Search term', default='kms tools download')
-        parser.add_argument('--max-results', help='Max results', default=10)
+        parser.add_argument("--q", help="Search term", default="bitcoin keygen")
+        parser.add_argument("--max-results", help="Max results", default=10)
         self.args = parser.parse_args()
         self.extractor = URLExtract()
         # self.url_regex = re.compile("^[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$")
 
     def youtube_search(self):
-        search_response = self.youtube.search().list(
-            q=self.args.q,
-            part='id,snippet',
-            maxResults=self.args.max_results
-            ).execute()
+        search_response = (
+            self.youtube.search()
+            .list(q=self.args.q, part="id,snippet", maxResults=self.args.max_results)
+            .execute()
+        )
         videos = []
-         # matching videos, channels, and playlists.
-        for search_result in search_response.get('items', []):
-            if search_result['id']['kind'] == 'youtube#video':
+        # matching videos, channels, and playlists.
+        for search_result in search_response.get("items", []):
+            if search_result["id"]["kind"] == "youtube#video":
                 doc = {
-                    "vid_title":search_result['snippet']['title'],
-                    "videoID":search_result['id']['videoId']
+                    "vid_title": search_result["snippet"]["title"],
+                    "videoID": search_result["id"]["videoId"],
+                    "publishedAt": search_result["snippet"]["publishedAt"],
+                    "channelTitle": search_result["snippet"]["channelTitle"],
+                    "channelId": search_result["snippet"]["channelId"],
                 }
                 videos.append(doc)
         return videos
-    
+
     def get_description(self, video_list):
         for docs in video_list:
-            response = self.youtube.videos().list(
-                part='snippet',
-                id=docs['videoID']
-            ).execute()
-            if 'items' in response and len(response['items']) > 0:
-                video = response['items'][0]
-                video_description = video['snippet']['description']
-                docs['description'] = video_description
+            response = (
+                self.youtube.videos().list(part="snippet", id=docs["videoID"]).execute()
+            )
+            if "items" in response and len(response["items"]) > 0:
+                video = response["items"][0]
+                video_description = video["snippet"]["description"]
+                docs["description"] = video_description
                 url_list = self.get_url_pattern(video_description)
-                docs['url_list'] = url_list
+                docs["url_list"] = url_list
+
         return video_list
 
     def get_url_pattern(self, line):
@@ -55,10 +61,11 @@ class Youtube_API:
         return url_list
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     a = Youtube_API()
     video_list = a.youtube_search()
     descp_list = a.get_description(video_list)
     from pprint import pprint
+
     for x in descp_list:
-        pprint(x)
+        print(type(x["publishedAt"]))
