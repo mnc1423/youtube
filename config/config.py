a4e3e5144c81cbcadf4d933449e2261e968a1156
mnc1423 <41675771+mnc1423@users.noreply.github.com>
2023-12-04 18:20:05 +0900

    updates to es handler

--- config/config.py
+++ config/config.py
@@ -1,13 +1,28 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
 
 def get_api_key() -> str:
     return os.getenv("API_KEY")
 
+
 def get_es_server() -> str:
     return os.getenv("ES_SERVER")
+
+
 def get_es_port() -> int:
     return os.getenv("ES_PORT")
+
+
 def get_es_index() -> str:
     return os.getenv("ES_INDEX")
+
+
+def get_es_user() -> str:
+    return os.getenv("ES_USER")
+
+
+def get_es_password() -> str:
+    return os.getenv("ES_PASS")
--- elastic_handler.py
+++ elastic_handler.py
@@ -6,7 +6,14 @@ class esHandler:
     def __init__(self):
         es_host = config.get_es_server()
         es_port = config.get_es_port()
-        self.es_client = Elasticsearch("localhost", port=es_port)
+        es_user = config.get_es_user()
+        es_password = config.get_es_password()
+        self.es_client = Elasticsearch(
+            hosts=[{"host": "172.18.0.2", "port": 9200, "scheme": "https"}],
+            http_auth=(es_user, es_password),
+            verify_certs=False,
+        )
+
         self.es_index = config.get_es_index()
 
     def create_doc(self, data):
@@ -27,3 +34,36 @@ class esHandler:
         for docs in _list:
             doc = self.create_doc(docs)
             inputList.append(doc)
+            return inputList
+
+    def generate_documents(self, _list):
+        for doc in _list:
+            yield doc
+
+    def esInput(self, _list):
+        success, failed = 0, 0
+        for ok, result in helpers.streaming_bulk(
+            client=self.es_client, actions=self.generate_documents(_list)
+        ):
+            if not ok:
+                failed += 1
+            else:
+                success += 1
+        print(success, failed)
+
+    def testDummyData(self):
+        doc = {
+            "url_list": "test_url",
+            "vid_title": "test_title",
+            "publishedAt": "test_date",
+            "channelTitle": "test_title",
+            "channelId": "test_channel",
+        }
+        result = self.es_client.index(index="test", id=1, body=doc)
+        # res = self.es_client.get(index="test", id="1")
+        print(result)
+
+
+if __name__ == "__main__":
+    a = esHandler()
+    a.testDummyData()
